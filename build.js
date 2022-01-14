8c2b17551e25c19d299f0386adff5ba3b1132367
dainel.zhang <dainel.zhang@silksoftware.com>
2022-01-14 17:24:48 +0800

    将命令式迁移到class方式

---
+++ build.js
@@ -0,0 +1,14 @@
+const index = require('./index');
+
+const rootPath = process.argv[2];
+const table = process.argv[3];
+const moduleName = process.argv[4];
+const backendUrl = process.argv[5];
+const location = process.argv[6];
+
+index.model.buildModelTemplateFiles(rootPath, moduleName, table)
+    .then(() => console.log('model done'))
+    .catch(err => {throw err});
+index.backend(rootPath, moduleName, table, backendUrl, location)
+    .then(() => console.log('backend done'))
+    .catch(err => {throw err});
