85a3c0e65815315ef803f1d7a00eb4212f085ce9
dainel.zhang <dainel.zhang@silksoftware.com>
2022-02-11 17:32:41 +0800

    引入 express

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-module.exports = {
-}
+const fs = require('fs');
+const express = require('express');
+const path = require("path");
+const MagentoBackendGrid = require('./libs/MagentoBackendGrid');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Express started on http://localhost:${port};`))
+
+app.get('/', (req, res) => {
+    console.log(new Date());
+    res.status(200);
+    res.type('text/html');
+    res.send('hello world!')
+})
+
+app.get('/grid', async (req, res) => {
+    const gridHtmlPath = path.join('html', 'grid.html');
+    res.type('text/html');
+    res.send(await fs.promises.readFile(gridHtmlPath))
+})
+
+app.post('/grid', async (req, res) => {
+
+})
+
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
+})
